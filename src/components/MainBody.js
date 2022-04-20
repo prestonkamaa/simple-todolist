12ddf829dc1d13cec26ee0d60f6db0748cff6abe
Preston <prestonkamaa@gmail.com>
2022-04-21 01:17:22 +0300

    Added new stylish icons and color

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,94 +1,73 @@
 import React from "react" 
 import { MdOutlineDeleteOutline } from "react-icons/md"; 
 import { MdDriveFileRenameOutline } from "react-icons/md";  
+import { MdOutlineCheckCircleOutline } from "react-icons/md";
+import { MdPanoramaFishEye } from "react-icons/md";
 
 export default function MainBody(){
+
+    // const[toDos,setToDos] = useState(false)
+
+    // function doneItems(){
+    //     setToDos((prevState) => {
+    //         !prevState
+    //     })
+    // } 
+
+function handleClick(){
+
+}
+
     return( 
-            <div className="main-body"> 
+            <div className="main-body">
               <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
-                <p>Directing or performing Website updates.  </p>
+                <MdOutlineCheckCircleOutline className="checkbox-icon-done" />  
+                <p onClick={handleClick}>Directing or performing Website updates.  </p>
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
-                </div>
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
+              </div>
+              <div className="checkbox">
+                <MdPanoramaFishEye className="checkbox-icon" />  
+                <p onClick={handleClick}>Edit, write and designing website content.  </p>
+                <MdDriveFileRenameOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
+              </div>   
                 <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
+                <MdPanoramaFishEye className="checkbox-icon" /> 
                 <p>Validating  form test.</p>                 
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
                 </div> 
               <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
+                <MdPanoramaFishEye className="checkbox-icon" /> 
                 <p>Edit, write and designing website content.</p>                
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
                  
                 </div>
                 <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
+                <MdPanoramaFishEye className="checkbox-icon" />  
                 <p>Back up files from Websites for recovery.</p>
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
                 </div> 
               <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
+                <MdPanoramaFishEye className="checkbox-icon" /> 
                 <p>Directing or performing Website updates.  </p>
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
                 </div>
                 <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
+                <MdPanoramaFishEye className="checkbox-icon" /> 
                 <p>Validating  form test.</p>                 
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
                 </div> 
               <div className="checkbox">
-                <input 
-                    type="checkbox"
-                    name="toDoItems"
-                    value=""
-                  //  onChange={e} 
-                  className="checkbox"
-                /> 
+                <MdPanoramaFishEye className="checkbox-icon" /> 
                 <p>Edit, write and designing website content.</p>                
                 <MdDriveFileRenameOutline className="icons" /> 
-                <MdOutlineDeleteOutline className="icons" /> 
+                <MdOutlineDeleteOutline className="icons delete-icon" /> 
                  
                 </div>  
                 <hr />
